ed41318476cae148fa305c0037db934af2353ecb
fedorniak <serhiy2005sss@gmail.com>
2022-10-15 00:03:50 +0300

    add

--- Homework/HW-1/HW-1/Source.cpp
+++ Homework/HW-1/HW-1/Source.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main()
 {
-	/*//ввести радіус і вивести довжину кола площу і об'єм
+	/*//enter radius print L S V
 		const float pi = 3.1415;
 			int r;
 			cout << "Enter radius:" << endl;
@@ -12,8 +12,7 @@ int main()
 			cout << "S=" << pi * pow(r,2) << endl;
 			cout << "V=" << 4.0/ 3 * (pi * pow(r, 3));*/
 
-			//ввести 2 натуральих вивести середнє арифметичне і геометричне
-	int num1, num2;
+			
 
 	cout << "Enter num1:" << endl;
 	cin >> num1;
